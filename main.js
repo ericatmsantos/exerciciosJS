bd43eedfcc2ae5d47d0e436def99b50ee3d36cd2
Érica <122843248+ericatmsantos@users.noreply.github.com>
2024-10-16 19:29:34 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,84 @@
+/*
+
+//Exercício 01
+alert('Hello World!')
+
+//Exercício 02
+const number1 = 27
+const number2 = 3
+
+const sum = number1 + number2
+
+alert (sum)
+
+//Exercício 03
+const isNumber = 7
+
+if (typeof isNumber == 'number') {
+  alert('É um número')
+} else {
+  alert('Não é um número')
+}
+
+//Exercício 04
+
+const isString = 'texto'
+
+if (typeof isString == 'string') {
+  alert ('É uma string')
+} else {
+  alert('Não é uma string')
+}
+
+//Exercício 05
+let isBoolean = true
+
+if (typeof isBoolean == 'boolean') {
+  alert ('É um booleano')
+} else {
+  alert('Não é um booleano')
+}
+
+//Exercício 06
+let number1 = 10
+let number2 = 2
+
+let sub = (number1 - number2)
+
+alert(sub)
+
+//Exercício 07
+let number1 = 10
+let number2 = 2
+
+let mult = (number1 * number2)
+
+alert(mult)
+
+//Exercício 08
+let number1 = 10
+let number2 = 2
+
+let div = (number1 / number2)
+
+alert(div)
+
+//Exercício 09
+let isEven = 8
+
+if (isEven % 2 == 0) {
+  alert('É um número par.')
+} else {
+  alert ('Não é um número par.')
+}
+
+//Exercício 10
+let isOdd = 5
+
+if (isOdd % 2 != 0) {
+  alert('É um número ímpar.')
+} else {
+  alert ('Não é um número ímpar.')
+}
+  
+*/
